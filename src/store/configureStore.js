ca0295b0b6685d69c47009827aebed46ebb79eb2
Vitor <catchtherainbow33@gmail.com>
2022-06-17 18:07:54 -0300

    Token e User reducers feitos e adicionados a store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import photo from "./photo";
+import token from "./token";
+import user from "./user";
 
 const middleware = () => (defaultMiddleware) => {
   return [...defaultMiddleware()]
 };
 
-const reducer = combineReducers({photo});
+const reducer = combineReducers({photo, token, user });
 const store = configureStore({reducer, middleware: middleware()});
 
 export default store
--- src/store/helper/createAsyncSlice.js
+++ src/store/helper/createAsyncSlice.js
@@ -1,4 +1,3 @@
-// importa o createSlice
 import { createSlice } from '@reduxjs/toolkit';
 
 /**
@@ -10,19 +9,14 @@ import { createSlice } from '@reduxjs/toolkit';
  * @param {Function} config.fetchConfig
  */
 const createAsyncSlice = (config) => {
-  // cria um slice
   const slice = createSlice({
-    // define um nome específico para o slice
     name: config.name,
-    // o estado inicial possui propriedades específicas
-    // mas podemos adicionar novas / escrever por cima das existentes
     initialState: {
       loading: false,
       data: null,
       error: null,
       ...config.initialState,
     },
-    // lista de reducers padrões
     reducers: {
       fetchStarted(state) {
         state.loading = true;
@@ -37,29 +31,25 @@ const createAsyncSlice = (config) => {
         state.data = null;
         state.error = action.payload;
       },
-      // novos reducers caso necessário
       ...config.reducers,
     },
   });
 
-  // desestruturação das ações
   const { fetchStarted, fetchSuccess, fetchError } = slice.actions;
-  // ação assíncrona (thunk), recebe um payload
+  
   const asyncAction = (payload) => async (dispatch) => {
     try {
       dispatch(fetchStarted());
-      // config.fetchConfig é um método que retorna
-      // o url e as opções do fetch
       const { url, options } = config.fetchConfig(payload);
       const response = await fetch(url, options);
       const data = await response.json();
+      if(response.ok === false) throw new Error(data.message)
       return dispatch(fetchSuccess(data));
     } catch (error) {
       return dispatch(fetchError(error.message));
     }
   };
 
-  // a função retorna as propriedades de slice e a ação assíncrona
   return { ...slice, asyncAction };
 };
 
