c5d0f70aad5368599ecdd9c625dedb3cfdb58571
Vitor <catchtherainbow33@gmail.com>
2022-06-17 18:13:12 -0300

    Login de usuário conectado ao reducer user na store

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom'
 import useForm from '../../Hooks/useForm'
 import Button from '../Form/Button'
 import Input from '../Form/Input'
-import { UserContext } from '../../UserContext'
 import Error from '../Helper/Error'
 import styles from './LoginForm.module.css'
 import stylesBtn from '../Form/Button.module.css'
 import Head from '../Helper/Head'
+import { useDispatch, useSelector } from 'react-redux'
+import { userLogin } from '../../store/user'
 
 const LoginForm = () => {
   const username = useForm()
   const password = useForm()
-  const {userLogin, error, loading} = React.useContext(UserContext)
+  const dispatch = useDispatch();
+  const {token, user} = useSelector(state => state);
+  const loading = token.loading || user.loading;
+  const error = token.error || user.error;
 
   async function handleSubmit(event) {
     event.preventDefault()
     if(username.validade() && password.validade()) {
-      userLogin(username.value,password.value)
+      dispatch(userLogin({username: username.value,password: password.value}))
     } 
   }
 
