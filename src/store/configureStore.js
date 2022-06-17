d45fac3d22045bd2f8c828c4b8cc692796c997ed
Vitor <catchtherainbow33@gmail.com>
2022-06-17 20:11:51 -0300

    PhotoPost reducer adicionado a store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,12 +4,14 @@ import token from "./token";
 import user from "./user";
 import feed from "./feed";
 import ui from "./ui";
+import photoPost from "./photoPost";
+
 
 const middleware = () => (defaultMiddleware) => {
   return [...defaultMiddleware()]
 };
 
-const reducer = combineReducers({photo, token, user, feed, ui });
+const reducer = combineReducers({photo, token, user, feed, ui, photoPost });
 const store = configureStore({reducer, middleware: middleware()});
 
 export default store
