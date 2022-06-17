b80d22e64d7eb60fcdf0ef026d37ddb686521f7c
Vitor <catchtherainbow33@gmail.com>
2022-06-17 18:36:10 -0300

    Botão de logout consertado

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { NavLink, useLocation } from 'react-router-dom'
-import { UserContext } from '../../UserContext'
 import { ReactComponent as MinhasFotos} from '../../Assets/feed.svg'
 import { ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg'
 import { ReactComponent as AdicionarFoto} from '../../Assets/adicionar.svg'
 import { ReactComponent as Sair} from '../../Assets/sair.svg'
 import styles from './UserHeaderNav.module.css'
 import useMedia from '../../Hooks/useMedia'
+import { userLogout } from '../../store/user'
+import { useDispatch } from 'react-redux'
 
 const UserHeaderNav = () => {
   const mobile = useMedia('(max-width:40rem)')
-  const {userLogout} = React.useContext(UserContext)
+  const dispatch = useDispatch();
   const [mobileMenu, setMobileMenu] = React.useState(false)
   const {pathname} = useLocation()
 
@@ -44,7 +45,7 @@ const UserHeaderNav = () => {
           <AdicionarFoto />
           {mobile && 'Adicionar foto'}
         </NavLink>
-        <button onClick={userLogout}>
+        <button onClick={() => dispatch(userLogout())}>
           <Sair />
           {mobile && 'Sair'}
         </button>
