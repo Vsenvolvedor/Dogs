efdb811db0ca6784bc8e3a9bfb4acc3d6d44ff7f
Vitor <catchtherainbow33@gmail.com>
2022-06-17 18:23:03 -0300

    Login funcionando junto a store

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const ProtectedRoute = (props) => {
-
+const ProtectedRoute = ({children}) => {
   const {data} = useSelector(state => state.user);
 
-  if(data) return <Route {...props} />;
+  if(data) return children;
   else if(data === null) return <Navigate to='/login' />;
   else return null
 }
